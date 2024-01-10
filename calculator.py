793857b85126450eb8deb04852fe811fb4f4020c
rosedaily <duwls1001@naver.com>
2024-01-10 17:39:54 +0900

    Add get_Median function

--- calculator.py
+++ calculator.py
@@ -1,7 +1,12 @@
 ##내용을 수정해보자
 def add(a+b);
-return a+b
+    return a+b
+
 def subtract(a,b);
-return a-b
+    return a-b
+
 def divide(a,b);
-return a/b
+    return a/b
+
+def get_Median(a,b);
+    return (a+b)/2
