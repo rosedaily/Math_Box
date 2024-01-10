7b42574b18e9125b687054baa17aa0d6b463424b
rosedaily <duwls1001@naver.com>
2024-01-10 16:04:49 +0900

    Remove say_hello function in calculator.py

--- calculator.py
+++ calculator.py
@@ -1,12 +1,12 @@
 ##내용을 수정해보자
 def add(a+b);
-return a+b
+    return a+b
+
 def subtract(a,b);
-return a-b
+    return a-b
+
 def divide(a,b);
-return a/b
+    return a/b
 
 def divide_new(a, b):
     return a/b
-def say_hello();
-    print("hello")
