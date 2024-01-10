7900009e1e4cd790abd882b4777e94fbd24b520a
rosedaily <duwls1001@naver.com>
2024-01-10 16:10:23 +0900

    Add square function

--- calculator.py
+++ calculator.py
@@ -1,10 +1,15 @@
 ##내용을 수정해보자
 def add(a+b);
-return a+b
+    return a+b
+
 def subtract(a,b);
-return a-b
+    return a-b
+    
 def divide(a,b);
-return a/b
+    return a*b
 
 def divide_free(a, b):
     return a/b
+
+def square(a);
+    return a*a
